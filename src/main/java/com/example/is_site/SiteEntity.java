948fc0ae590d7201507ad688bf6218fc18070789
Sebastian <129107648+sebandrien@users.noreply.github.com>
2024-01-29 18:14:39 -0500

    Update SiteEntity.java

--- src/main/java/com/example/is_site/SiteEntity.java
+++ src/main/java/com/example/is_site/SiteEntity.java
@@ -3,6 +3,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Column;
+import jakarta.persistence.*;
 
 @Entity
 public class SiteEntity {
