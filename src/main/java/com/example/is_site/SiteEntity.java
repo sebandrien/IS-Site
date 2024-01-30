a46e98c7937c591d3be384333ecede3e19e21445
unknown <sebandrien@gmail.com>
2024-01-29 20:05:29 -0500

    new

--- src/main/java/com/example/is_site/SiteEntity.java
+++ src/main/java/com/example/is_site/SiteEntity.java
@@ -1,4 +1,8 @@
 package com.example.is_site;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
 import jakarta.persistence.*;
 
 @Entity
